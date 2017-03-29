3ca9cd0b16beefc3f9bbb9174c6ac5807826d624
melardev <melardev@gmail.com>
2017-03-30 00:35:13 +0200

    Manage Users

--- app/src/main/java/com/melardev/tutorialsfirebase/MainActivity.java
+++ app/src/main/java/com/melardev/tutorialsfirebase/MainActivity.java
@@ -13,9 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new AlertDialog.Builder(this).setMessage("Please note that the demos may not work, because I have added a sha1 fingerprint in my firebase console," +
+        /*new AlertDialog.Builder(this).setMessage("Please note that the demos may not work, because I have added a sha1 fingerprint in my firebase console," +
                 " the code shown in my tutorials in youtube was tested and it doest work, take it as a template and copy it to your android studio project, " +
-                "update the values in strings.xml accordingly, search me in youtube by searching Melar Dev").setPositiveButton(android.R.string.ok, null).setCancelable(false).show();
+                "update the values in strings.xml accordingly, search me in youtube by searching Melar Dev").setPositiveButton(android.R.string.ok, null).setCancelable(false).show();*/
     }
 
     public void registerDemo(View view) {
@@ -59,10 +59,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startDemo(Class className) {
-        startActivity(new Intent(this,className));
+        startActivity(new Intent(this, className));
     }
 
     public void dbObjMap(View view) {
         startDemo(ActivityDBRetriever.class);
+    }
+
+    public void manageUser(View view) {
+        startDemo(ActivityUpdateUser.class);
     }
 }
